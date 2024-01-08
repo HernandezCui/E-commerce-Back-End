0f3d36ed72a740c139de094bc7160e32101cdb43
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-07 16:26:07 -0800

    finished router to delete product by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -131,8 +131,25 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const product = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!product) {
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Product deleted successfuly' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
