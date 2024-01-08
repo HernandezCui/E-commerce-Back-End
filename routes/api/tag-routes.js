7eed50e4dff558394a63a317af6754f51af1a566
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-07 18:18:46 -0800

    finished router to delete tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -78,8 +78,24 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagsData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagsData) {
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Tag deleted successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
